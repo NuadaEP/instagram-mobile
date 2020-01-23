c78f9db7b2117cb2b302ee1261f1666c03219507
Bruno Cardoso <bruno@liveonsolutions.com>
2020-01-22 23:57:22 -0200

    making the tabview style

--- src/index.js
+++ src/index.js
@@ -1,7 +1,61 @@
 import "react-native-gesture-handler";
-import React from "react";
-import { Text } from "react-native";
+import React, { Component } from "react";
+import { Dimensions } from "react-native";
+import { TabView, SceneMap, TabBar } from "react-native-tab-view";
 
-const App = () => <Text>Hi</Text>;
+import Gallery from "./pages/Gallery";
+import Photo from "./pages/Photo";
+import Video from "./pages/Video";
 
-export default App;
+export default class App extends Component {
+  state = {
+    index: 0,
+    routes: [
+      {
+        key: "gallery",
+        title: "Galeria"
+      },
+      {
+        key: "photo",
+        title: "Foto"
+      },
+      {
+        key: "video",
+        title: "Vídeo"
+      }
+    ]
+  };
+
+  renderScene = SceneMap({
+    gallery: Gallery,
+    photo: Photo,
+    video: Video
+  });
+
+  renderTabBar = props => (
+    <TabBar
+      {...props}
+      activeColor="black"
+      inactiveColor="gray"
+      indicatorStyle={{ backgroundColor: "black" }}
+      style={{ backgroundColor: "white" }}
+      labelStyle={{ fontSize: 14 }}
+    />
+  );
+
+  render() {
+    const initialLayout = { width: Dimensions.get("window").width };
+
+    return (
+      <TabView
+        navigationState={this.state}
+        renderScene={this.renderScene}
+        onIndexChange={index => this.setState({ index })}
+        initialLayout={initialLayout}
+        swipeEnabled={true}
+        tabBarPosition="bottom"
+        renderTabBar={props => this.renderTabBar(props)}
+      />
+    );
+  }
+}
