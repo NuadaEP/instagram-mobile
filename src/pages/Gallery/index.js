17e5d47e7fccb1ecd022be5930947fe8258efb80
Bruno Cardoso <bruno@liveonsolutions.com>
2020-01-22 22:31:12 -0200

    creating routes file and pages

---
+++ src/pages/Gallery/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { View, Text } from "react-native";
+
+// import { Container } from './styles';
+
+const Gallery = () => (
+  <View>
+    <Text>Gallery</Text>
+  </View>
+);
+
+export default Gallery;
---
+++ src/pages/Photo/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { View, Text } from "react-native";
+
+// import { Container } from './styles';
+
+const Photo = () => (
+  <View>
+    <Text>Photo</Text>
+  </View>
+);
+
+export default Photo;
