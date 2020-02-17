e0350f30c078925fb8580566b8883641e28f61c3
Daniel de Souza Neris <danielneris01@gmail.com>
2020-02-17 08:21:10 -0300

    add alert na pg de publish e arrumando crash quando nao tinha img

--- src/components/Publish/index.js
+++ src/components/Publish/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TextInput, TouchableOpacity, Image } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, Image, Alert } from 'react-native';
 import CameraRollHeader from "../../components/CameraRollHeader";
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
@@ -10,12 +10,28 @@ export default function Publish({ navigation }) {
 
   const { state: { params } } = navigation;
 
+  handleFunction = () => {
+    Alert.alert(
+      'Publish',
+      'You will publish this now, are you sure?',
+      [
+        {
+          text: 'Cancel',
+          onPress: () => { },
+          style: 'cancel',
+        },
+        { text: 'OK', onPress: () => { } },
+      ],
+      { cancelable: false },
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <CameraRollHeader title="Publish" />
+      <CameraRollHeader title="Publish" handleFunction={handleFunction} />
       <View style={styles.inputsContainer}>
         <View style={styles.inputView}>
-          <Image source={{ uri: params.response }} style={styles.imagePreviewContainer} />
+          {params && <Image source={{ uri: params.response }} style={styles.imagePreviewContainer} />}
           <TextInput
             style={styles.input}
             placeholder="Write a thing..."
@@ -30,10 +46,6 @@ export default function Publish({ navigation }) {
 
         <TouchableOpacity style={styles.button}>
           <Text>Add Location</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.button}>
-          <Text>Also post to</Text>
         </TouchableOpacity>
       </View>
     </View>
